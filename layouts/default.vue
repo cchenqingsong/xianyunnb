2e48fabbcf26b84bd1a48e432354e2764cb1a66a
陈清松 <821570533@qq.com>
2020-02-07 19:33:33 +0800

    '脚手架生成项目，基本文件夹完成'

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header></Header>
+    <nuxt />
+  </div>
+</template>
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+export default {
+  components: {
+    Header,Footer
+  }
+}
+</script>
+<style>
+
+</style>
