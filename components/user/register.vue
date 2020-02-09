e715b764581b22ad558d8358ae62160e272af6b1
陈清松 <821570533@qq.com>
2020-02-09 18:01:04 +0800

    '注册表单输入框动态数据绑定'

--- components/user/register.vue
+++ components/user/register.vue
@@ -1,91 +1,77 @@
 <template>
-    <el-form 
-        :model="form" 
-        ref="form" 
-        :rules="rules" 
-        class="form">
-            <el-form-item class="form-item">
-                <el-input 
-                placeholder="用户名手机">
-                </el-input>
-            </el-form-item>
-            <el-form-item class="form-item">
-                <el-input 
-                placeholder="验证码" >
-                    <template slot="append">
-                        <el-button @click="handleSendCaptcha">
-                            发送验证码
-                        </el-button>
-                    </template>
-                </el-input>
-            </el-form-item>
-            <el-form-item class="form-item">
-                <el-input 
-                placeholder="你的名字">
-                </el-input>
-            </el-form-item>
-            <el-form-item class="form-item">
-                <el-input 
-                placeholder="密码" 
-                type="password"
-                ></el-input>
-            </el-form-item>
-            <el-form-item class="form-item">
-                <el-input 
-                placeholder="确认密码" 
-                type="password">
-                </el-input>
-            </el-form-item>
-            <el-button 
-            class="submit" 
-            type="primary" 
-            @click="handleRegSubmit">
-                注册
-            </el-button>
-        </el-form>
+  <el-form :model="form" ref="form" :rules="rules" class="form">
+    <el-form-item class="form-item" prop="username">
+      <el-input placeholder="用户名手机" v-model="form.username"></el-input>
+    </el-form-item>
+    <el-form-item class="form-item" prop="captcha">
+      <el-input placeholder="验证码" v-model="form.captcha">
+        <template slot="append">
+          <el-button @click="handleSendCaptcha">
+            发送验证码
+          </el-button>
+        </template>
+      </el-input>
+    </el-form-item>
+    <el-form-item class="form-item" prop="nickname">
+      <el-input placeholder="你的名字" v-model="form.nickname"> </el-input>
+    </el-form-item>
+    <el-form-item class="form-item" prop="password">
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item class="form-item" prop="checkPassword">
+      <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"> </el-input>
+    </el-form-item>
+    <el-button class="submit" type="primary" @click="handleRegSubmit">
+      注册
+    </el-button>
+  </el-form>
 </template>
 <script>
 export default {
-    data(){
-        return {
-            // 表单数据
-            form: {},
-            // 表单规则
-            rules: {},
-        }
-    },
-    methods: {
-        // 发送验证码
-        handleSendCaptcha(){
+  data() {
+    return {
+      // 表单数据
+      form: {
+          username: '',
+          captcha: '',
+          nickname: '',
+          password: '',
+          checkPassword: ''
+      },
+      // 表单规则
+      rules: {
+          
+      }
+    };
+  },
+  methods: {
+    // 发送验证码
+    handleSendCaptcha() {},
 
-        },
-
-        // 注册
-        handleRegSubmit(){
-           
-        }
-    }
-}
+    // 注册
+    handleRegSubmit() {}
+  }
+};
 </script>
 
 <style scoped lang="less">
-    .form{
-        padding:25px;
-    }
+.form {
+  padding: 25px;
+}
 
-    .form-item{
-        margin-bottom:20px;
-    }
+.form-item {
+  margin-bottom: 20px;
+}
 
-    .form-text{
-        font-size:12px;
-        color:#409EFF;
-        text-align: right;
-        line-height: 1;
-    }
+.form-text {
+  font-size: 12px;
+  color: #409eff;
+  text-align: right;
+  line-height: 1;
+}
 
-    .submit{
-        width:100%;
-        margin-top:10px;
-    }
+.submit {
+  width: 100%;
+  margin-top: 10px;
+}
 </style>
