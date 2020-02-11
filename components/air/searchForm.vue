ab2224116d9298766425c68684798d23de34d40c
陈清松 <821570533@qq.com>
2020-02-11 21:32:53 +0800

    '完成input输入框搜索功能'

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="search-form">
-
         <!-- 头部tab切换 -->
         <el-row type="flex" class="search-tab">
             <span v-for="(item, index) in tabs" :key="index"
@@ -9,12 +8,12 @@
                 <i :class="item.icon"></i>{{item.name}}
             </span>
         </el-row>
-
         <el-form class="search-form-content" ref="form" label-width="80px">
             <el-form-item label="出发城市">
                 <!-- fetch-suggestions 返回输入建议的方法 -->
                 <!-- select 点击选中建议项时触发 -->
                 <el-autocomplete
+                v-model="form.departCity"
                 :fetch-suggestions="queryDepartSearch"
                 placeholder="请搜索出发城市"
                 @select="handleDepartSelect"
@@ -23,6 +22,7 @@
             </el-form-item>
             <el-form-item label="到达城市">
                 <el-autocomplete
+                v-model="form.destCity"
                 :fetch-suggestions="queryDestSearch"
                 placeholder="请搜索到达城市"
                 @select="handleDestSelect"
@@ -32,6 +32,7 @@
             <el-form-item label="出发时间">
                 <!-- change 用户确认选择日期时触发 -->
                 <el-date-picker type="date" 
+                v-model="form.departDate"
                 placeholder="请选择日期" 
                 style="width: 100%;"
                 @change="handleDate">
@@ -51,7 +52,6 @@
         </el-form>  
       </div>
 </template>
-
 <script>
 export default {
     data(){
@@ -61,6 +61,15 @@ export default {
                 {icon: "iconfont iconshuangxiang", name: "往返"}
             ],
             currentTab: 0,
+            form:{
+                departCity:'',
+                departCode:'',
+                destCity:'',
+                destCode:'',
+                departDate:'',
+            },
+            departData :[],
+            destData:[]
         }
     },
     methods: {
@@ -68,47 +77,73 @@ export default {
         handleSearchTab(item, index){
             
         },
-        
         // 出发城市输入框获得焦点时触发
         // value 是选中的值，cb是回调函数，接收要展示的列表
-        queryDepartSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
+        queryDepartSearch(value, callback){
+            if(!value){
+                return;
+            }
+            // 调用axios根据输入的城市获取城市的信息
+            this.$axios({
+                url:'/airs/city',
+                params:{
+                    name: this.form.departCity
+                }
+            }).then(res=>{
+                // 返回的数据没有所要的value：数据的格式，所以，数据改造
+                const newData = res.data.data.map(item=>{
+                        // replace方法将字符串里面的 ‘市’，替换为空，目标是去掉市
+                        item.value = item.name.replace('市','')
+                        return item
+                    })
+                // 将newData存起来
+                this.departData = newData
+                // 这个callback是组件自带的传参，目的是让获得的数据展示在下拉列表中
+                callback(newData);
+            })
+            
         },
-
         // 目标城市输入框获得焦点时触发
-        // value 是选中的值，cb是回调函数，接收要展示的列表
-        queryDestSearch(value, cb){
-            cb([
-                {value: 1},
-                {value: 2},
-                {value: 3},
-            ]);
+        // value 是选中的值，callback是回调函数，接收要展示的列表
+        queryDestSearch(value, callback){
+                    // 目标城市一样的，和起始城市一样
+                if(!value){
+                    return;
+                }
+                // 调用axios根据输入的城市获取城市的信息
+                this.$axios({
+                    url:'/airs/city',
+                    params:{
+                        name: this.form.destCity
+                    }
+                }).then(res=>{
+                    // 返回的数据没有所要的value：数据的格式，所以，数据改造
+                    const newData = res.data.data.map(item=>{
+                            item.value = item.name.replace('市','')
+                            return item
+                        })
+                    // 将newData存起来
+                    this.destData = newData
+                    // 这个callback是组件自带的传参，目的是让获得的数据展示在下拉列表中
+                    callback(newData);
+                })
         },
-       
         // 出发城市下拉选择时触发
         handleDepartSelect(item) {
             
         },
-
         // 目标城市下拉选择时触发
         handleDestSelect(item) {
             
         },
-
         // 确认选择日期时触发
         handleDate(value){
-           
+        //    引入插件moment
         },
-
         // 触发和目标城市切换时触发
         handleReverse(){
             
         },
-
         // 提交表单是触发
         handleSubmit(){
            
