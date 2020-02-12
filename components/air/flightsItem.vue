0220146a1b6d9c609f0a8135d35c84de85950c96
陈清松 <821570533@qq.com>
2020-02-12 23:52:48 +0800

    分页功能的完成

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -64,7 +64,7 @@ export default {
     },
     data () {
         return {
-            isShow: true
+            isShow: false
         }
     },
     computed: {
--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -14,9 +14,26 @@
                 
                 
                 <!-- 航班信息 -->
-                <flightsItem v-for="(item,index) in flightsData" :key="index" :data='item'/>
-            </div>
+                <flightsItem v-for="(item,index) in dataList" :key="index" :data='item'/>
 
+                <!-- 分页组件 -->
+                    <el-row type="flex" justify="center" style="margin-top:10px;">
+                        <!-- size-change：切换条数时候触发 -->
+                        <!-- current-change：选择页数时候触发 -->
+                        <!-- current-page: 当前页数 -->
+                        <!-- page-size：当前条数 -->
+                        <!-- total：总条数 -->
+                        <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="pageIndex"
+                        :page-sizes="[5, 10, 15, 20]"
+                        :page-size="pageSize"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="total">
+                        </el-pagination>
+                    </el-row>
+            </div>
             <!-- 侧边栏 -->
             <div class="aside">
                 <!-- 侧边栏组件 -->
@@ -33,7 +50,7 @@ import flightsItem from '@/components/air/flightsItem'
 export default {
     data(){
         return {
-            // 机票总数据 （有info, flights, total, options这些属性）
+            // 机票数据,是一个对象
             flightsData: {},
             // 当前页数
             pageIndex: 1,
@@ -46,6 +63,29 @@ export default {
     components: {
         flightsListHead,flightsItem
     },
+    methods: {
+        // 切换条数
+        // 默认参数value的值就是当前切换的条数
+        handleSizeChange(value){
+            this.pageSize = value
+        },
+        // 切换当前页数
+        // 默认参数value的值就是当前页数
+        handleCurrentChange(value){
+            this.pageIndex = value
+        }
+    },
+    computed: {
+        // 计算属性，为了在前端实现数据的分页    
+        dataList(){
+            // 判断flightsData有没有值
+            if(!this.flightsData.flights){
+                return [] ;
+            }
+            // 0-5   5-10  10-15    
+            return this.flightsData.flights.slice((this.pageIndex-1)*this.pageSize,this.pageIndex*this.pageSize)
+        }
+    },
     mounted () {
         // 获取服务器的数据
         // console.log(this.$route.query)
@@ -54,8 +94,9 @@ export default {
             params: this.$route.query
         }).then(res=>{
             // console.log(res)
-            this.flightsData = res.data.flights
-            console.log(this.flightsData)
+            this.flightsData = res.data
+            this.total = res.data.total
+            // console.log(this.flightsData)
         })
     }
 }
