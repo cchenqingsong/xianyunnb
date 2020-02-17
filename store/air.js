da07530fc3be6af61415d94daa38407f0b533fef
陈清松 <821570533@qq.com>
2020-02-17 22:21:34 +0800

    '解决搜索历史的bug'

--- store/air.js
+++ store/air.js
@@ -13,7 +13,9 @@ export const mutations = {
     setHistory(state,data){
         state.history.unshift({...data})
         // 将数据的长度固定，数据将自动截取五个
-        state.history.length = 5
+        if(state.history.length > 5){
+            state.history.length = 5
+        }
     },
     // 保存订单信息
     setPlaneData(state,data){
